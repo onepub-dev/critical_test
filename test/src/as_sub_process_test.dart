f118d3d03158f75747d608dc1637a93310713368
Brett Sutton <bsutton@noojee.com.au>
2021-05-12 06:38:51 +1000

    formatting.

--- test/src/as_sub_process_test.dart
+++ test/src/as_sub_process_test.dart
@@ -3,7 +3,6 @@ import 'package:test/test.dart';
 
 void main() {
   test('Run critical_test as a spawned process', () {
-
-    'critical_test'.start(workingDirectory: pwd , nothrow: true);
+    'critical_test'.start(workingDirectory: pwd, nothrow: true);
   });
 }
