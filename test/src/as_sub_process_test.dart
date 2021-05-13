8849854f6981c782461fb067bb9e298b8014315f
Brett Sutton <bsutton@noojee.com.au>
2021-05-13 17:00:02 +1000

    stopped this test causing infinite recusion by stopping it spawning itself.

--- test/src/as_sub_process_test.dart
+++ test/src/as_sub_process_test.dart
@@ -1,8 +1,9 @@
+@Timeout(Duration(minutes: 3))
 import 'package:dcli/dcli.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Run critical_test as a spawned process', () {
-    'critical_test'.start(workingDirectory: pwd, nothrow: true);
+    'critical_test --tags="bad"'.start(workingDirectory: pwd, nothrow: true);
   });
 }
